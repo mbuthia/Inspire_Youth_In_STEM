904ab443f2db43d2a05fa2a57d08e49071d54207
unknown <gloriawambo@gmail.com>
2023-02-14 22:22:59 -0800

    my first python code

---
+++ Sermons and Outlines_files/strings.py
@@ -0,0 +1,12 @@
+#String concerntanation
+message="Hello"
+name="Gloria"
+greetings=message+" "+name
+print(greetings)
+#How to convert strings from upper case to lower case
+name="Wambo
+print(name.lower())
+city="nairobi"
+print(city.upper())
+
+
