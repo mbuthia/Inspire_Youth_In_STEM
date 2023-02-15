93aeb4655e77f7f5dcd8a341774230d54c2a6e70
unknown <gloriawambo@gmail.com>
2023-02-14 23:58:09 -0800

    initial commit

---
+++ Week-2/for_loop.py
@@ -0,0 +1,23 @@
+for i in range(0,7) :
+    print(i)
+for i in range(0,20) :
+    print(i)
+    sum=0
+    for i in range(1,10) :
+        sum=sum+i
+        print(sum)
+        #print product of first ten numbers
+        print(sum)
+        product=1
+        for x in range(1,10) :
+            product=product * x
+            print(product)
+            
+            
+            
+
+
+    
+    
+
+
